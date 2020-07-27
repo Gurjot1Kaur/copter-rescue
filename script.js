c0a6f22e8f640129750003654969a8f7521553c8
Gurjot1Kaur <gurjot.pkaur@gmail.com>
2020-07-27 10:31:07 -0700

    updates

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var counter=0;
 
 //Event Listeners
 hole.addEventListener('animationiteration',()=>{
-    var random=((Math.random()*300)+150);/*game screen is visible for block if it is between -150 and -450 because we already have it on 500 positionat the first iteration */
+    var random=((Math.random()*300)+250);/*game screen is visible for block if it is between -250 and -450 because we already have it on 500 positionat the first iteration */
     hole.style.top=-1*random+"px";
     counter++;//counts each animation
 });
@@ -22,7 +22,7 @@ setInterval(function(){
     var blockLeft=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop=parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     var cTop=-(500-characterTop);// taking charactertop value in negative because holetop is in negative
-    if((characterTop>480)||((blockLeft<50)&&(blockLeft>-50)&&((cTop+50>holeTop+150)||(cTop<holeTop)))){/* ball has hit the bottom or the block is hitting the wall  */
+    if((characterTop>480)||((blockLeft<50)&&(blockLeft>-50)&&((cTop+50>holeTop+250)||(cTop<holeTop)))){/* ball has hit the bottom or the block is hitting the wall  */
         alert("Game Over. Score: "+ counter);
         character.style.top= 100+"px";//so that the character is at bottoon reload
         counter=0;
@@ -35,7 +35,7 @@ function jump(){
     var jumpInterval=setInterval(function(){
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));/*gets the current top position of the character div whichis going to be 100px because that is what we set it to*/
         if(characterTop>6 && jumpCount<15){/*Dont add to the top if characterTop<6 this means dont jump if it is on the border of the screen */
-            character.style.top=characterTop-3 + "px";
+            character.style.top=characterTop-5 + "px";
         }
         
         if(jumpCount>20){
